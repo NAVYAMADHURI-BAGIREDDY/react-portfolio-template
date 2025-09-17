3bb1bf4c1e1e2c1699f1d275fc78c7c4fae6ef83
NAVYAMADHURI BAGIREDDY <reddynavya1602@gmail.com>
2025-09-16 21:33:20 -0500

    Header and footer updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Main.scss';
+import '../assets/styles/Main.scss'
+import NavyaAvatar from '../assets/images/Navya.png';
+;
 
 function Main() {
 
@@ -9,19 +11,20 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+        <img src={NavyaAvatar} alt="Avatar" />
+
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/NAVYAMADHURI-BAGIREDDY" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/navyamadhuri/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>Navya Madhuri</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/NAVYAMADHURI-BAGIREDDY" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/navyamadhuri/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
